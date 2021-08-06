8c4d378769fcd950c080d530922f4acd2501a6c1
Ego <augurto.17@gmail.com>
2021-08-06 05:29:27 -0500

    prueba 2

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -3,4 +3,5 @@
 	mysql_connect("localhost","u415020159_constructora","Constructora*#17");
 	mysql_select_db("u415020159_constructora");
  /* prueba de sincronizacion */
+ /* prueba 2 de sincornizacion  */
 ?>
