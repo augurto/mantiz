6407047154cb149f0f49965c008e29504c3c55e4
Ego <augurto.17@gmail.com>
2021-08-06 10:17:00 -0500

    contrasena de bd era eguito

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-	mysql_connect("localhost","u415020159_mantiz","Constructora*#17");
+	mysql_connect("localhost","u415020159_mantiz","Eguito*#17");
 	mysql_select_db("u415020159_mantiz");
  /* prueba de sincronizacion */
  /* prueba 2 de sincornizacion  */
