c4cc33024d788537ca013af746bd84df351b84ed
Ego <augurto.17@gmail.com>
2021-08-06 07:07:08 -0500

    agregando dni 70668404 a la exepcion para no poder editar

--- ajax/buscar_miembros.php
+++ ajax/buscar_miembros.php
@@ -29,7 +29,7 @@ session_start();
 		
 	}
 	if($action == 'ajax'){
-		$sql="SELECT * FROM  usuarios where dni not in (0)"; /* agregar dni para que no puedan editarlo */
+		$sql="SELECT * FROM  usuarios where dni not in ('0','70668404')"; /* agregar dni para que no puedan editarlo */
 		$query = mysqli_query($con, $sql);
 			?>
 			<div class="card shadow mb-4">
