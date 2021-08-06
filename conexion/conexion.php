27aedcc23ae39d3b25331ae057386cbb27c7e572
Ego <augurto.17@gmail.com>
2021-08-06 10:16:17 -0500

    cambio de bd

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,7 +1,7 @@
 <?php
 
-	mysql_connect("localhost","u415020159_constructora","Constructora*#17");
-	mysql_select_db("u415020159_constructora");
+	mysql_connect("localhost","u415020159_mantiz","Constructora*#17");
+	mysql_select_db("u415020159_mantiz");
  /* prueba de sincronizacion */
  /* prueba 2 de sincornizacion  */
 ?>
