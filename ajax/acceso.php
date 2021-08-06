48676ad1feb307bc869de17df4a3a6704fa9c5d8
Ego <augurto.17@gmail.com>
2021-08-06 06:00:50 -0500

    cambio id proyecto por estado

--- ajax/acceso.php
+++ ajax/acceso.php
@@ -9,7 +9,7 @@ require_once ("../config/conexion.php");
 if(isset($_POST["username"]) && isset($_POST["password"])){
   $username = mysqli_real_escape_string($con, $_POST["username"]);
   $password = mysqli_real_escape_string($con,  sha1($_POST["password"]));
-  $sql = "SELECT username,id,rol, codigo_proyecto FROM usuarios WHERE username='$username' AND password='$password' GROUP BY username";
+  $sql = "SELECT username,id,rol, estado FROM usuarios WHERE username='$username' AND password='$password' GROUP BY username";
   $result = mysqli_query($con, $sql);
   $num_row = mysqli_num_rows($result);
   if ($num_row == "1") {
@@ -17,7 +17,7 @@ if(isset($_POST["username"]) && isset($_POST["password"])){
     $_SESSION["username"] = $data["username"];
 	$_SESSION["id_user"] = $data["id"];
     $_SESSION["prol"] = $data["rol"];
-     $_SESSION["codigo"] = $data["codigo_proyecto"];
+     $_SESSION["codigo"] = $data["estado"];
     echo "1";
   } else {
     echo "error";
